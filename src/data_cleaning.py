8f19198909dce908830e76f6798d6dc5c343f8b2
Ben Morgan <bm618@exeter.ac.uk>
2026-03-21 09:48:11 +0000

    Added raw datasets from ONS

---
+++ src/data_cleaning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+#Checking to find the path
+#import os
+#print(os.getcwd())
+# Importing the dataset
+wages = pd.read_excel('C:\Users\paul\OneDrive\Ben\Uni\Year 2\Data Science\data\Raw_Data\Median_Weekly_Earnings.xlsx')
+print(wages.head())
